7ba2db95c457bdeb02b26422fd914e6433ba99a9
Phuong Nguyen <vanphuong12a2@gmail.com>
2018-06-05 19:39:11 +0200

    volume converter

--- src/main/scala/UnitConverter.scala
+++ src/main/scala/UnitConverter.scala
@@ -1,23 +1,29 @@
-abstract class Unit(val conversionFactorFromMeter: Double)
-object Meter extends Unit(1)
-object Centimeter extends Unit(0.01)
-object Inch extends Unit(0.0254)
+abstract class Unit[T](val conversionFactorFromBaseUnit: Double)
 
-case class Length(value: Double, unit: Unit) {
+trait LengthUnit
+object Meter extends Unit[LengthUnit](1)
+object Centimeter extends Unit[LengthUnit](0.01)
+object Inch extends Unit[LengthUnit](0.0254)
 
-  def add(otherLength: Length) = {
-    val resultValue = otherLength.valueInMeter + valueInMeter
-    Length(resultValue, Meter)
+trait VolumeUnit
+object Liter extends Unit[VolumeUnit](1)
+object Gallon extends Unit[VolumeUnit](3.78)
+
+case class Measure[T](private val value: Double, private val unit: Unit[T]) {
+
+  def add(otherMeasure: Measure[T]) = {
+    Measure(otherMeasure.valueInBaseUnit / unit.conversionFactorFromBaseUnit + value, unit)
   }
 
-  private lazy val valueInMeter = value * unit.conversionFactorFromMeter
+  private lazy val valueInBaseUnit = value * unit.conversionFactorFromBaseUnit
 
   override def hashCode(): Int = super.hashCode()
 
   override def equals(obj: scala.Any): Boolean = {
     obj match {
-      case otherLength: Length => otherLength.valueInMeter == valueInMeter
+      case otherLength: Measure[T] => otherLength.valueInBaseUnit == valueInBaseUnit
       case _ => false
     }
   }
+
 }
